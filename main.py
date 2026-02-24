c8313ea3a26ba9a69b373c5ae9c3803e988dc8f4
rosignol08 <rchaffray@gmail.com>
2026-02-24 15:24:37 +0100

    cleaning

--- main.py
+++ main.py
@@ -55,9 +55,6 @@ max_width = 600
 max_height = 600
 
 root.minsize(min_width, min_height)
-#root.maxsize(max_width, max_height)
-
-#root.attributes('-topmost', 2)
 
 root.attributes('-alpha',1) #si je veut mettre en transparent
 
@@ -68,18 +65,6 @@ try:
     root.iconphoto(False, photo)
 except tk.TclError:
     print("icon file not found.")
-    
-    
-    
-
-'''
-si on veut des flag pour desactiver ou pas le bouton
-# set the disabled flag
-button.state(['disabled'])
-
-# remove the disabled flag
-button.state(['!disabled'])
-'''
 
 # exit button
 exit_button = ttk.Button(
@@ -89,30 +74,9 @@ exit_button = ttk.Button(
 )
 exit_button.grid(column=1, row=7, columnspan=3, sticky='nsew')
 
-
-#exit_button.pack(
-#    ipadx=5,
-#    ipady=5,
-#    expand=True
-#)
-
 def button_clicked():
     print('Button clicked')
 
-
-#ttk.Button(root, text='Click Me',command=button_clicked)
-
-#button = ttk.Button(root, text='Click Me', command=button_clicked)
-#button.pack()
-
-'''bouton avec image pour telechargre la video'''
-
-
-#def handle_click():
-#    showinfo(
-#        title='Information',
-#        message='Download button clicked!'
-#    )
 
 #la deuxième fenetre
 download_icon = tk.PhotoImage(file='./assets/vdo.png')
@@ -144,42 +108,8 @@ download_button.grid(column=3, row=3, columnspan=5, rowspan=2, sticky='nsew')
 def select(truc):
     print(truc)
 
-#boutton_fonction = ttk.Button(root, text='Rock', command=lambda: select('Rock')).pack()
-
-
 font=("Helvetica", 14)
 
 
 message = tk.Label(root, text="compression de video", font=font).grid(column=3, row=1, columnspan=5, rowspan=2, sticky='nsew')
-
-#message1 = tk.Label(root, text='Classic Label')
-#message2 = ttk.Label(root, text='Themed Label')
-
-#photo = tk.PhotoImage(file='./assets/MSI_MPG.png')
-#image_label = ttk.Label(
-#    root,
-#    image=photo,
-#    padding=5,
-#    text='Python',
-#    compound=tk.TOP
-#)
-#image_label.pack()
-
-#message1.pack()
-#message2.pack()
-
-
-'''pour placer des widget a certain endroits
-
-label1 = tk.Label(master=root, text="Place",bg='red',fg='white')
-label1.place(x=0,y=0,width=120, height=60)
-
-'''
-
-#pour ouvrir un fichier file_path = filedialog.askopenfilename(title="Select a File")
-
-
 root.mainloop()
-
-
-#"./assets/269091_video-camera-icon.png"
